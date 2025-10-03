baa75a4400e26975256b7c5987d9ac4296594bb1
myeonghoon <mhoo999@naver.com>
2025-10-03 20:18:50 +0900

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -3,3 +3,18 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import TodoList from './pages/TodoList';
 import About from './pages/About';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/todos' element={<TodoList />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;

14dde4abfd9de96937e3fe219a06953717c759c2
myeonghoon <mhoo999@naver.com>
2025-10-03 20:16:44 +0900

    add about page

---
+++ src/pages/About.tsx
@@ -0,0 +1,15 @@
+function About() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>소개</h1>
+      <p>React + Vite + Zustand + TypeScript로 만든 Todo 앱입니다.</p>
+      <ul>
+        <li>⚡ Vite: 빠른 개발 서버</li>
+        <li>🐻 Zustand: 간단한 전역 상태관리</li>
+        <li>🔷 TypeScript: 타입 안정성</li>
+      </ul>
+    </div>
+  );
+}
+
+export default About;
